89f6ca7bee3cb6d2aeebfd3c447dab8ed216a02c
hk <zgupie@gmail.com>
2017-10-07 17:29:30 +0200

    UPDATE: added more tokens for lex an

--- main.cpp
+++ main.cpp
@@ -13,21 +13,51 @@ extern char *yytext;
 //vraci nazev enumu (kvuli prehlednejsimu vypisu)
 const char* ename(enum ETokenType e) {
 	switch (e) {
-		case T_TYPE_INT: return "T_TYPE_INT";
-		case T_TYPE_CHAR: return "T_TYPE_CHAR";
-		case T_TYPE_STRING: return "T_TYPE_STRING";
-		case T_TYPE_VOID: return "T_TYPE_VOID";
-		case T_LPAR: return "T_LPAR";
-		case T_RPAR: return "T_RPAR";
-		case T_LCUR: return "T_LCUR";
-		case T_RCUR: return "T_RCUR";
-		case T_SEMI: return "T_SEMI";
-		case T_ASSI: return "T_ASSI";
-		case T_COMMA: return "T_COMMA";
-		case T_ID: return "T_ID";
-		case T_INT: return "T_INT";
-		case T_CHAR: return "T_CHAR";
-		case T_ERR: return "T_ERR";   
+	case T_TYPE_INT: return "T_TYPE_INT";
+	case T_TYPE_CHAR: return "T_TYPE_CHAR";
+	case T_TYPE_STRING: return "T_TYPE_STRING";
+	case T_TYPE_VOID: return "T_TYPE_VOID";
+
+	case T_TYPE_SHORT: return "T_TYPE_SHORT";
+	case T_TYPE_UNSIGNED: return "T_TYPE_UNSIGNED";
+
+	case T_IF: return "T_IF";
+	case T_ELSE: return "T_ELSE";
+	case T_WHILE: return "T_WHILE";
+	case T_FOR: return "T_FOR";
+	case T_BREAK: return "T_BREAK";
+	case T_CONTINUE: return "T_CONTINUE";
+	case T_RETURN: return "T_RETURN";
+
+	case T_LPAR: return "T_LPAR";
+	case T_RPAR: return "T_RPAR";
+	case T_LCUR: return "T_LCUR";
+	case T_RCUR: return "T_RCUR";
+	
+	case T_NEG: return "T_NEG"; // !
+	case T_MUL: return "T_MUL"; // *
+	case T_DIV: return "T_DIV"; // /
+	case T_MOD: return "T_MOD"; // %
+	case T_PLUS: return "T_PLUS"; // +
+	case T_MINUS: return "T_MINUS"; // -
+	case T_LT: return "T_LT"; // <
+	case T_GT: return "T_GT"; // >
+	case T_LEQ: return "T_LEQ"; // <=
+	case T_GEQ: return "T_GEQ"; // >=
+	case T_EQ: return "T_EQ"; // ==
+	case T_NEQ: return "T_NEQ"; // !=
+	case T_AND: return "T_AND"; // &&
+	case T_OR: return "T_OR"; // ||
+
+	case T_SEMI: return "T_SEMI"; // ;
+	case T_ASSI: return "T_ASSI"; // =
+	case T_COMMA: return "T_COMMA"; //
+	
+	case T_ID: return "T_ID";
+	case T_INT: return "T_INT";
+	case T_CHAR: return "T_CHAR";
+	case T_STRING: return "T_STRING";
+	case T_ERR: return "T_ERR";  
 	}
 }
 
--- tokenTypes.h
+++ tokenTypes.h
@@ -5,16 +5,47 @@ enum ETokenType{
 	T_TYPE_CHAR,
 	T_TYPE_STRING,
 	T_TYPE_VOID,
+
+	T_TYPE_SHORT,
+	T_TYPE_UNSIGNED,
+
+	T_IF,
+	T_ELSE,
+	T_WHILE,
+	T_FOR,
+	T_BREAK,
+	T_CONTINUE,
+	T_RETURN,
+
 	T_LPAR,
 	T_RPAR,
 	T_LCUR,
 	T_RCUR,
-	T_SEMI,
-	T_ASSI,
-	T_COMMA,
+	
+	T_NEG, // !
+	T_MUL, // *
+	T_DIV, // /
+	T_MOD, // %
+	T_PLUS, // +
+	T_MINUS, // -
+	T_LT, // <
+	T_GT, // >
+	T_LEQ, // <=
+	T_GEQ, // >=
+	T_EQ, // ==
+	T_NEQ, // !=
+	T_AND, // &&
+	T_OR, // ||
+
+	T_SEMI, // ;
+	T_ASSI, // =
+	T_COMMA, // ,
+	
 	T_ID,
 	T_INT,
 	T_CHAR,
+	T_STRING,
 	T_ERR
+	
 };
 #endif
