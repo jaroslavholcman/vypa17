de9ca5306d253876f5d1af257428a899aae73ecb
hk <zgupie@gmail.com>
2017-10-08 13:45:23 +0200

    UPDATE: refactor

--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 EXECUTABLE="vypa17"
-RULES_FLEX="rules.l"
-RULES_BISON="rules.y"
-SRC_FLEX="rules.l.c"
-SRC_BISON="rules.b.c"
+
+SCANNER_L="scanner.l"
+SCANNER_C="scanner.c"
+SCANNER_O="scanner.o"
+
+PARSER_L="parser.y"
+PARSER_C="parser.c"
+PARSER_H="parser.h"
+
 
 function removeFileIfExists {
 	if [ -f $1 ]; then
@@ -12,17 +17,18 @@ function removeFileIfExists {
 
 function clean {
 	removeFileIfExists $EXECUTABLE
-	removeFileIfExists $SRC_FLEX
+	removeFileIfExists $SCANNER_C
 	rm -rf *.o
 }
 
 clean
 
 echo "##COMPILE######################################################"
-flex -o $SRC_FLEX $RULES_FLEX
-gcc -c $SRC_FLEX
+flex -o $SCANNER_C $SCANNER_L
+gcc -c $SCANNER_C
+
 g++ -c main.cpp
-g++ main.o rules.l.o -o $EXECUTABLE
+g++ main.o $SCANNER_O -o $EXECUTABLE
 echo "###############################################################"
 
 if [ -f $EXECUTABLE ]; then
